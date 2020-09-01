7a02996b4912392be9d5847408e75dfbcb940ef8
Dipjul <dipjulrahman2@gmail.com>
2020-09-01 11:01:05 +0530

    Update and rename DemoApplication.java to Application.java

--- application/src/main/java/com/dipjul/application/Application.java
+++ application/src/main/java/com/dipjul/application/Application.java
@@ -16,10 +16,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @Bean
